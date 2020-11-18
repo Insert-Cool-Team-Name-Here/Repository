495474a3dfb59b17f16c006995260768f552e63a
tyabbi <ToBiSanchez25@gmail.com>
2020-11-17 20:25:22 -0800

    Delete DivideMethodTest.java

--- JustinRobinson/src/test/java/DivideMethodTest.java
+++
@@ -1,9 +0,0 @@
-import JUnitAssignment;
-import org.junit.*;
-public class DivideMethodTest{
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void divideByZeroShouldThrowException(){
-		divide(5,0);
-	}
-}
